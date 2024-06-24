e30e78cebbb3e273378b84429b2a1d81448ba97c
kanghaeun <xgkg0330@jnu.ac.kr>
2024-06-25 00:16:00 +0900

    feat: supabase data 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-const App = () => {
-  return <div></div>;
-};
+import { useEffect, useState } from "react";
+import { supabase } from "./supabase";
+
+function App() {
+  const [postData, setPostData] = useState([]);
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  async function getPostData() {
+    const { data } = await supabase.from("page").select("*");
+    setPostData(data);
+    console.log(postData);
+  }
+  return <></>;
+}
 
 export default App;
