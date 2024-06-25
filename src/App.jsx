a82fc2d84f24da924e85931519725b2e2e97f3bc
kanghaeun <xgkg0330@jnu.ac.kr>
2024-06-26 06:59:00 +0900

    feat: Main,ContentDisplay 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
+import styled from "styled-components";
+import ContentDisplay from "./components/ContentDisplay";
+import Main from "./components/Main";
 
 function App() {
   const [postData, setPostData] = useState([]);
+  const [selectedId, setSelectedId] = useState("");
+
+  const [selectedTitle, setSelectedTitle] = useState("");
+  const [selectedTag, setSelectedTag] = useState("");
+  const [selectedBody, setSelectedBody] = useState("");
 
   useEffect(() => {
     getPostData();
@@ -11,9 +19,35 @@ function App() {
   async function getPostData() {
     const { data } = await supabase.from("page").select("*");
     setPostData(data);
-    console.log(postData);
   }
-  return <></>;
+  const handlePostClick = (id, title, tag, body) => {
+    setSelectedTitle(title);
+    setSelectedTag(tag);
+    setSelectedBody(body);
+    setSelectedId(id);
+  };
+  return (
+    <>
+      <Frame>
+        <Main
+          handlePostClick={handlePostClick}
+          getPostData={getPostData}
+          postData={postData}
+        />
+        <ContentDisplay
+          title={selectedTitle}
+          tag={selectedTag}
+          body={selectedBody}
+          id={selectedId}
+          getPostData={getPostData}
+        />
+      </Frame>
+    </>
+  );
 }
 
 export default App;
+
+const Frame = styled.div`
+  display: flex;
+`;
